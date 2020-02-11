0723c215fed6f3d0695ba33e44ec992410567afb
cxf <1579814534@qq.com>
2020-02-11 22:53:47 +0800

    nothing

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 导入全局样式表
+import './assets/css/global.css'
+
+// element-ui按需引入
+import './plugins/element'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 图标库
+import './assets/fonts/iconfont.css'
+
 Vue.config.productionTip = false
 
 new Vue({
