b22ba2828f541d9192ec6d7d09e974a9c60855ff
cxf <1579814534@qq.com>
2020-02-12 00:32:29 +0800

    完成登陆模块开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,57 +1,64 @@
 <template>
-    <el-container class="home-container">
-        <!-- 头部区域 -->
-        <el-header>
-            <div>
-                <img src="../assets/logo.png"
-                     alt="">
-                <span>vue_lms</span>
-            </div>
-            <el-button type="info"
-                       @click="logout">退出</el-button>
-        </el-header>
-        <!-- 页面主体区域 -->
-        <el-container>
-            <!-- 侧边栏 -->
-            <el-aside :width="isCollapse ? '64px' : '200px'">
-                <div class="toggle-button"
-                     @click="toggleCollapse">|||</div>
-                <el-menu background-color="#333744"
-                         text-color="#fff"
-                         active-text-color="#409EFF"
-                         unique-opened
-                         :collapse="isCollapse"
-                         :collapse-transition="false"
-                         router
-                         :default-active="activePath">
-                    <!-- 一级菜单 -->
-                    <el-submenu :index="item.id +''"
-                                v-for="item in menulist"
-                                :key="item.id">
-                        <template slot="title">
-                            <i :class="iconsObj[item.id]"></i>
-                            <span>{{item.authName}}</span>
-                        </template>
-                        <!-- 二级菜单 -->
-                        <el-menu-item @click="saveNavState('/' + subItem.path)"
-                                      :index="'/' + subItem.path"
-                                      v-for="subItem in item.children"
-                                      :key="subItem.id">
-                            <template slot="title">
-                                <i class="el-icon-menu"></i>
-                                <span>{{subItem.authName}}</span>
-                            </template>
-                        </el-menu-item>
-                    </el-submenu>
-                </el-menu>
-            </el-aside>
-            <!-- 右侧内容主体 -->
-            <el-main>
-                <!-- router占位符 -->
-                <router-view></router-view>
-            </el-main>
-        </el-container>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/logo.png"
+             alt="">
+        <span>华软社团管理系统</span>
+      </div>
+      <el-button type="info"
+                 @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button"
+             @click="toggleCollapse">|||</div>
+        <!-- 侧边栏菜单区域 -->
+        <el-menu background-color="#333744"
+                 text-color="#fff"
+                 active-text-color="#409EFF"
+                 unique-opened
+                 :collapse="isCollapse"
+                 :collapse-transition="false"
+                 router
+                 :default-active="activePath">
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id + ''"
+                      v-for="item in menulist"
+                      :key="item.id">
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <!-- <i :class="iconsObj[item.id]"></i> -->
+              <!-- 文本 -->
+              <span>{{item.authName}}</span>
+            </template>
+
+            <!-- 二级菜单 -->
+            <el-menu-item :index="'/' + subItem.path"
+                          v-for="subItem in item.children"
+                          :key="subItem.id"
+                          @click="saveNavState('/' + subItem.path)">
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-menu"></i>
+                <!-- 文本 -->
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
     </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -60,15 +67,14 @@ export default {
     return {
       // 左侧菜单数据
       menulist: [],
-      // 一级菜单图标
-      iconsObj: {
-        '125': 'iconfont icon-user',
-        '103': 'iconfont icon-tijikongjian',
-        '101': 'iconfont icon-shangpin',
-        '102': 'iconfont icon-danju',
-        '145': 'iconfont icon-baobiao'
-      },
-      // 菜单是否折叠
+      // iconsObj: {
+      //   '125': 'iconfont icon-user',
+      //   '103': 'iconfont icon-tijikongjian',
+      //   '101': 'iconfont icon-shangpin',
+      //   '102': 'iconfont icon-danju',
+      //   '145': 'iconfont icon-baobiao'
+      // },
+      // 是否折叠
       isCollapse: false,
       // 被激活的链接地址
       activePath: ''
@@ -76,29 +82,28 @@ export default {
   },
   created() {
     this.getMenuList()
-    this.activePath = window.sessionStorage.getItem('actionPath')
+    this.activePath = window.sessionStorage.getItem('activePath')
   },
   methods: {
-    // 退出
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
     },
-    // 获取所有菜单
+    // 获取所有的菜单
     async getMenuList() {
-      const { data: res } = await this.$http.get('menus')
-      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
-      this.menulist = res.data
+      const { data: res } = await this.$http.post('sys/profile')
+      if (res.meta.status !== 10000) return this.$message.error(res.message)
+      // this.menulist = res.data
       console.log(res)
     },
-    // 菜单折叠与展开
+    // 点击按钮，切换菜单的折叠与展开
     toggleCollapse() {
       this.isCollapse = !this.isCollapse
     },
     // 保存链接的激活状态
-    saveNavState(actionPath) {
-      window.sessionStorage.setItem('actionPath', actionPath)
-      this.actionPath = actionPath
+    saveNavState(activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+      this.activePath = activePath
     }
   }
 }
@@ -119,27 +124,27 @@ export default {
   > div {
     display: flex;
     align-items: center;
-    img {
-      height: 40px;
-      width: 40px;
-    }
     span {
       margin-left: 15px;
     }
   }
 }
+
 .el-aside {
   background-color: #333744;
   .el-menu {
-    border: none;
+    border-right: none;
   }
 }
+
 .el-main {
   background-color: #eaedf1;
 }
+
 .iconfont {
   margin-right: 10px;
 }
+
 .toggle-button {
   background-color: #4a5064;
   font-size: 10px;
--- src/main.js
+++ src/main.js
@@ -12,6 +12,17 @@ import 'element-ui/lib/theme-chalk/index.css'
 // 图标库
 import './assets/fonts/iconfont.css'
 
+import axios from 'axios'
+// 配置请求的跟路径
+axios.defaults.baseURL = 'http://127.0.0.1:9002/'
+axios.interceptors.request.use(config => {
+  // console.log(config)
+  config.headers.Authorization = 'Bearer ' + window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
